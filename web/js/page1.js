27c4cac4465c020a8cf4ea279ad04dc1c1c2ac46
Tosiq <pnymeet@gmail.com>
2026-08-30 13:04:27 +0500

    js intro

---
+++ web/js/page1.js
@@ -0,0 +1,16 @@
+// alert(1+1)
+
+// console.log(1 + 1);
+
+var age = 25;
+var name = "Tosiq";
+var accountStatus = false;
+var account_balance = true;
+//  object    key    value
+var user = {
+  name: "Asad",
+  email: "asad@email.com",
+  birthMark: null,
+  // birthMark: { place: "chin", color: "black" }
+}; // curly bracket
+console.log(user.name);
