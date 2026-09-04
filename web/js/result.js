2c3ac065d27260a4cb8ea99bca85f2097b133895
Tosiq <pnymeet@gmail.com>
2026-09-04 13:11:00 +0500

    arithmatic operation == result

---
+++ web/js/result.js
@@ -0,0 +1,10 @@
+var s1 = 89
+var s2 = 65
+var s3 = 99
+var totalMarks = 300
+
+var obtainedMarks = s1 + s2 + s3
+var percentage = (obtainedMarks / totalMarks) * 100
+
+console.log("Obtained marks: ", obtainedMarks)
+console.log("Percentage: ", percentage.toFixed(2))
