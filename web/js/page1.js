4f4624c6208fe4ea72d6fa0cf9e85d481df227dc
Tosiq <pnymeet@gmail.com>
2026-09-04 12:54:31 +0500

    js types remaining

--- web/js/page1.js
+++ web/js/page1.js
@@ -4,8 +4,9 @@
 
 var age = 25;
 var name = "Tosiq";
-var accountStatus = false;
-var account_balance = true;
+var accountStatus = false; // camelCase
+var account_balance = true; // snake_case
+// console.log(account_balance)
 //  object    key    value
 var user = {
   name: "Asad",
@@ -13,4 +14,27 @@ var user = {
   birthMark: null,
   // birthMark: { place: "chin", color: "black" }
 }; // curly bracket
-console.log(user.name);
+// console.log(user.email)
+var d = new Date()
+// console.log(d.getDay(), d.getDate(), d.getMonth()+1, d.getFullYear());
+
+var colors = ["Purple", "Pink", "Black", "White"]
+
+console.log(colors)
+console.log(colors[3])
+
+// const matrix = [
+//   [1, 2, 3], // Row 0
+//   [4, 5, 6], // Row 1
+//   [7, 8, 9]  // Row 2
+// ];
+
+// console.log(matrix[2][2])
+
+showData()
+function showData () {
+  console.log(name, age)
+}
+
+var validateEmail = /^\S+@\S+\.\S+$/
+
