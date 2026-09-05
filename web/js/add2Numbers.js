85f93cf80c4beb55c2b6d5276dd3ebc852bb7cfa
tosiq <pnymeet@gmail.com>
2026-09-05 13:19:49 +0500

    get value from user

--- web/js/add2Numbers.js
+++ web/js/add2Numbers.js
@@ -1,7 +1,14 @@
 function addNumbers() {
     var a = Number(document.getElementById("no1").value)
     var b = Number(document.getElementById("no2").value)
-
+    
+    var res = document.getElementById("result")
+    var hed = document.getElementById("heading")
+    
     var sum = a + b
-    console.log(sum)
+
+    res.innerText = "Sum is: " + sum
+ 
+
+    console.log("Extracted heading from H1 tag", hed.innerText)
 }
