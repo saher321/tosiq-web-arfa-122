977fd0a0621cdbb7eeffad79351e33caadf5221a
tosiq <pnymeet@gmail.com>
2026-09-05 13:20:20 +0500

    .

--- web/js/add2Numbers.js
+++ web/js/add2Numbers.js
@@ -3,12 +3,9 @@ function addNumbers() {
     var b = Number(document.getElementById("no2").value)
     
     var res = document.getElementById("result")
-    var hed = document.getElementById("heading")
     
     var sum = a + b
 
     res.innerText = "Sum is: " + sum
- 
 
-    console.log("Extracted heading from H1 tag", hed.innerText)
 }
