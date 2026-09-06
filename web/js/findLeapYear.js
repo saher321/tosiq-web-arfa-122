fbaa8f7b2e7951c29b571856494a89c58e0b944c
Tosiq <pnymeet@gmail.com>
2026-09-06 13:19:10 +0500

    validation

--- web/js/findLeapYear.js
+++ web/js/findLeapYear.js
@@ -1,9 +1,16 @@
 function findLeapYear() {
     var year = Number(document.getElementById("year").value)
 
-    if (year % 4 == 0) { // true
-        console.log("It is leap year")
-    } else { // false
-        console.log("It is not leap year")
+    if (year == 0) {
+        alert("Please fill the field")
+        return;
     }
+    
+    var result = document.getElementById("result")
+    if (year % 4 == 0) { // true
+        result.innerText = "It is leap year"
+    } else { // false
+        result.innerText = "It is not leap year"
+    }
+
 }
