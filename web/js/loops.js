85a82e6c9bb8f8b2be30cad0e1d4ea3d3241011e
Tosiq <pnymeet@gmail.com>
2026-09-12 13:41:11 +0500

    loop

---
+++ web/js/loops.js
@@ -0,0 +1,36 @@
+// variable declaration
+// variable initialization
+// condition
+// inc, decr
+
+let i
+let sum = 0
+for (i = 1; i <= 10; i++) {
+    // console.log(i)
+    sum = sum + i
+    // let table = 2 + " x " + i + " = " + 2*i
+    // console.log(table)
+}
+// 1- 0 + 1 => sum = 1
+// 2- 1 + 2 => sum = 3
+// 10- sum + 10 => sum + 10 => total sum = 55
+// iterations
+// i = 1, 1 <= 10 :: print 1,     i++
+// i = 2, 2 <= 10 :: print 2
+// i = 10, 10 <= 10 :: print 10 , i++
+// i = 11, 11 <= 10 :: print 10
+
+console.log(sum)
+// i = 1, 1 <= 10 :: print 1
+// i = 2, 2 <= 10 :: print 2
+// i = 10, 10 <= 10 :: print 10 , i++
+// i = 11, 11 <= 10 :: print 10
+
+
+
+// output:
+// 2 x 1 = 2
+// 2 x 2 = 4
+
+
+
