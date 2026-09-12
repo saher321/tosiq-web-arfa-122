269893aaab0df9762e93f2411a87423cb5d9e95b
Tosiq <pnymeet@gmail.com>
2026-09-12 12:37:47 +0500

    if else, switch

---
+++ web/js/evenOdd.js
@@ -0,0 +1,9 @@
+function findEvenOdd (){
+    var number = Number(document.getElementById("number").value)
+
+    if (number % 2 == 0){
+        console.log("Even number")
+    } else {
+        console.log("Odd number")
+    }
+}
