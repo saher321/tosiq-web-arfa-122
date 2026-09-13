061f9c3dea3bb2c6b5a7babfd2f0f5e7911cc66b
Tosiq <pnymeet@gmail.com>
2026-09-13 12:48:37 +0500

    while loop

---
+++ web/js/loops2.js
@@ -0,0 +1,24 @@
+
+// declaration
+// initialization
+// condition
+// increament / decreament
+let sum = 0
+for (let i = 1; i <= 100; i++) {
+    // console.log(i,"I love pakistan")
+    sum = sum + i
+}
+console.log("Total", sum)
+
+let tblData = document.getElementById("table-data")
+let y = 1
+while(y <= 10) {
+    // 2 x 1 = 2
+    // 2 x 2 = 4
+    tblData.innerHTML += `
+    <tr>
+        <td> 2 x ${y} = ${2*y} </td>
+    </tr>
+    `
+    y++
+}
