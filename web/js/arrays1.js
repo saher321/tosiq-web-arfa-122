42da9154b53f5a9b16ae1a2728ed1fdc1547b4ac
Tosiq <pnymeet@gmail.com>
2026-09-19 13:40:03 +0500

    array1

---
+++ web/js/arrays1.js
@@ -0,0 +1,8 @@
+// index        0        1         2         3         4
+const arr1 = ["Blue", "Purple", "Indigo", "Yellow", "Black", "White"]
+// console.log(arr1[4])
+console.log(arr1.sort())
+
+for (let i=0; i < arr1.length; i++){
+    console.log(arr1[i])
+}
