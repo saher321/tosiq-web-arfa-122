26f33f0b2026a26cd033121b6ee05958db08b311
Tosiq <pnymeet@gmail.com>
2026-09-25 12:43:19 +0500

    display users in tbl

---
+++ web/js/users-list.js
@@ -0,0 +1,22 @@
+const users = [
+    {id: 1, name: "Alexa", email: "alexa@email.com", salary: 20000, status: "inactive"},
+    {id: 2, name: "Sam", email: "sam@email.com", salary: 70000, status: "active"},
+    {id: 3, name: "Micheal", email: "mic@email.com", salary: 210000, status: "active"},
+    {id: 4, name: "Freddy", email: "fredd@email.com", salary: 90000, status: "active"},
+]
+
+showUsers(users)
+function showUsers(users) {
+    let usersTbl = document.getElementById("users-tbl")
+    users.map((user) => {
+        usersTbl.innerHTML += `
+        <tr>
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.salary}</td>
+            <td>${user.status}</td>
+        </tr>
+        `
+    })
+}
